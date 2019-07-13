ac8ff6d684d66b1ca3586331e97021148ee8e903
orkunozgiray <36546670+orkunozgiray@users.noreply.github.com>
2019-07-13 00:38:33 -0400

    Navbar
    Navbar  has been updated

--- e-shopping/src/App.js
+++ e-shopping/src/App.js
@@ -16,10 +16,10 @@ class App extends Component {
       <React.Fragment>
         <Navbar />
         <Switch>
-          <Route path="/" component={ ProductList } />
+          <Route exact path="/" component={ ProductList } />
           <Route path="/details" component={ Details } />
           <Route path="/cart" component={ Cart } />
-          <Route component={ Default } />
+          <Route component={Default} />
         </Switch>
       </React.Fragment>
     );
--- e-shopping/src/components/ProductList.js
+++ e-shopping/src/components/ProductList.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import Product from './Product';
 
 export default class ProductList extends Component {
+    state={
+        products:[]
+    }
     render() {
         return (
-            <div>
-                <Product />
-            </div>
+            <React.Fragment>
+                
+            </React.Fragment>
         )
     }
 }
